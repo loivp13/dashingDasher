24371ceb6e16f5d6a1dc9cc70be59f2d75d4e633
Loi <loivp13@gmail.com>
2021-09-09 10:55:47 -0700

    refactor form into it' sown component

--- client/src/features/landingPage/LandingPage.js
+++ client/src/features/landingPage/LandingPage.js
@@ -3,90 +3,13 @@ import Grid from "@material-ui/core/Grid";
 import LandingPageHeroImage from "../../images/LandingHero.png";
 import ForgotPwHeroImage from "../../images/forgotPwPage.jpg";
 import SignupHeroImage from "../../images/signUpPage.jpg";
-import { Button } from "@material-ui/core";
 import useStyles from "./LandingPage.styles";
-import FormItems from "./components/FormItems";
-import FormNavigation from "./components/FormNavigation";
-import cloneDeep from "lodash/cloneDeep";
+import Form from "./components/Form/Form";
 
 export default function LandingPage() {
   const classes = useStyles();
   let [curView, setCurView] = useState("signin");
-  let [errorInfo, setErrorInfo] = useState({
-    signin: {},
-    signup: {},
-    forgotPw: {},
-  });
-  let [formInfo, setFormInfo] = useState({
-    signin: {
-      username: "",
-      password: "",
-    },
-    signup: {
-      username: "",
-      password: "",
-      email: "",
-    },
-    forgotPw: {
-      email: "",
-    },
-  });
-  const isValidateForm = (cloneErrorInfo) => {
-    let isValid = true;
-    for (let key in formInfo[curView]) {
-      if (!formInfo[curView][key]) {
-        cloneErrorInfo[curView][key] = `${key} is required`;
-        isValid = false;
-      }
-    }
-    return isValid;
-  };
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    let cloneErrorInfo = cloneDeep(errorInfo);
-    switch (curView) {
-      case "signin":
-        if (isValidateForm(cloneErrorInfo)) {
-          console.log("send http");
-        } else {
-          setErrorInfo(cloneErrorInfo);
-        }
-        break;
-      case "signup":
-        if (isValidateForm(cloneErrorInfo)) {
-          console.log("send http");
-        } else {
-          setErrorInfo(cloneErrorInfo);
-        }
-        break;
-      case "forgotPw":
-        if (isValidateForm(cloneErrorInfo)) {
-          console.log("send http");
-        } else {
-          setErrorInfo(cloneErrorInfo);
-        }
-        break;
-    }
-  };
-  const handleOnFormChange = (label, value) => {
-    let cloneFormInfo = cloneDeep(formInfo);
-    cloneFormInfo[curView][label] = value;
-    setFormInfo(cloneFormInfo);
-    setErrorInfo({
-      signin: {},
-      signup: {},
-      forgotPw: {},
-    });
-  };
-  const handleForgotPwClick = () => {
-    setCurView("forgotPw");
-  };
-  const handleSignUpClick = () => {
-    setCurView("signup");
-  };
-  const handleSignInClick = () => {
-    setCurView("signin");
-  };
+
   const renderOrderCss = (type) => {
     //ENABLE FLIPPING ORDER ON SIGNUP
     // if (curView === "signup" && type === "img") {
@@ -149,32 +72,7 @@ export default function LandingPage() {
         container
         className={classes.formContainer}
       >
-        <form
-          onSubmit={handleSubmit}
-          className={classes.form}
-          noValidate
-          autoComplete="off"
-        >
-          <FormItems
-            errorInfo={errorInfo}
-            handleOnFormChange={handleOnFormChange}
-            curView={curView}
-          ></FormItems>
-          <Button
-            className={classes.submitButton}
-            variant="contained"
-            color="primary"
-            type="submit"
-          >
-            {curView === "signin" ? "login" : "submit"}
-          </Button>
-          <FormNavigation
-            curView={curView}
-            handleSignUpClick={handleSignUpClick}
-            handleForgotPwClick={handleForgotPwClick}
-            handleSignInClick={handleSignInClick}
-          ></FormNavigation>
-        </form>
+        <Form curView={curView} setCurView={setCurView}></Form>
       </Grid>
     </Grid>
   );
